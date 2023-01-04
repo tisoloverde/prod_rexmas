88d1735caf1ce7cdeb99b9b5cc8ff2cfd1f41caa
Rodrigo Riveros <rodrigo.riveros@cryptodata.cl>
2023-01-04 06:45:49 -0300

    [update] fix catalogo 3

--- descarga_datos.php
+++ descarga_datos.php
@@ -681,7 +681,7 @@
 
       $codigo = $arreglo[$j][2];
       $nombre = $arreglo[$j][3];
-      if($arreglo[$j][7])[1])[0] !== ""){
+      if($arreglo[$j][7])[1] !== ""){
         $clasificacion = $JEAS[trim(explode("-",explode(")",$arreglo[$j][7])[1])[0])];
       }
       else{
