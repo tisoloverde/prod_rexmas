1488cbb532c3aa0fcc4656d24626b7e5fd1e3ee2
Rodrigo Riveros <rodrigo.riveros@cryptodata.cl>
2023-05-01 00:16:53 -0400

    [update] fix BD

--- conexion.php
+++ conexion.php
@@ -12,7 +12,7 @@
 		// Producción
 		$user = "AppConnect";
 		$pass = "TCw4etVPjEcqTRkGOqG8";
-		$db = "GENERICA";
+		$db = "SOLO_VERDE";
 		$host = "10.0.0.8";
 
 		mysqli_report(MYSQLI_REPORT_STRICT);
