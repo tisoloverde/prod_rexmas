f3417d9348987ba6d3742b6d168fe0b7e2e86b1d
Rodrigo Riveros <rodrigo.riveros@cryptodata.cl>
2023-06-23 03:28:01 -0400

    [update] set php

--- descarga_datos.php
+++ descarga_datos.php
@@ -9,7 +9,7 @@
   use PhpOffice\PhpSpreadsheet\IOFactory;
 
   // $ruta = 'C:\\xampp\\htdocs\\Git\\rexmas\\';
-  $ruta = '/var/www/html/generico/rexmas/';
+  $ruta = '/var/www/html/generico/rexmas/descargas/';
 
   echo "Hora de inicio: " . date('Y-m-d H:i:s') . "\n\n";
 
